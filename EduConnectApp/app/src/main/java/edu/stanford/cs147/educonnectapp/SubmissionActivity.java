33222a021b6d8af8470ea5e21c7a88341ed4491d
Sanura N'Jaka <sanura.njaka@DN0a1f2b14.SUNet>
2018-11-30 10:45:54 -0800

    added submission page and connected all current activities through button clicks

---
+++ EduConnectApp/app/src/main/java/edu/stanford/cs147/educonnectapp/SubmissionActivity.java
@@ -0,0 +1,13 @@
+package edu.stanford.cs147.educonnectapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SubmissionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_submission);
+    }
+}
---
+++ EduConnectApp/app/src/main/java/edu/stanford/cs147/educonnectapp/SubmittedActivity.java
@@ -0,0 +1,12 @@
+package edu.stanford.cs147.educonnectapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SubmittedActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
