ffa3541d7b60ed4f217e6e43d05b084003ab4a9e
Paulina <preyeshawaii@gmail.com>
2018-11-30 03:29:11 -0800

    Login functionality and ty for submitting screen

--- EduConnectApp/app/src/main/java/edu/stanford/cs147/educonnectapp/LoginActivity.java
+++ EduConnectApp/app/src/main/java/edu/stanford/cs147/educonnectapp/LoginActivity.java
@@ -5,11 +5,17 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     private TextView mTextMessage;
+    Button studentLogin;
+    EditText username, password;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -36,9 +42,30 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        studentLogin = (Button) findViewById(R.id.loginButtonStudent);
+        username= (EditText) findViewById(R.id.usernameField);
+        password= (EditText) findViewById(R.id.passwordField);
+        studentLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                login();
+            }
+        });
+
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+    public void login() {
+        String user=username.getText().toString().trim();
+        String pass=password.getText().toString().trim();
+        if(user.equals("jailenem") && pass.equals("ilovejailene"))
+        {
+            setContentView(R.layout.activity_class_list);
+        }else {
+            Toast.makeText(this, "Username and password do not match. Please try again.", Toast.LENGTH_LONG).show();
+        }
     }
 
 }
