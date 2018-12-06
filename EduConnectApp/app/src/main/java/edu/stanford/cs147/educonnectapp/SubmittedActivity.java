1c4cf18039869fcf7eed9aba1dc5a60f2348c282
Paulina <preyeshawaii@gmail.com>
2018-12-06 14:24:08 -0800

    teacher submission

--- EduConnectApp/app/src/main/java/edu/stanford/cs147/educonnectapp/SubmittedActivity.java
+++ EduConnectApp/app/src/main/java/edu/stanford/cs147/educonnectapp/SubmittedActivity.java
@@ -1,5 +1,6 @@
 package edu.stanford.cs147.educonnectapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,8 @@ public class SubmittedActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_submitted);
+        // PUT THIS IN DetailedSRResults.java IN ONCREATE:
+        Intent intent = getIntent();
+        String descriptionText = intent.getExtras().getString("descriptionText");
     }
 }
--- EduConnectApp/app/src/main/java/edu/stanford/cs147/educonnectapp/TeacherSubmissionActivity.java
+++ EduConnectApp/app/src/main/java/edu/stanford/cs147/educonnectapp/TeacherSubmissionActivity.java
@@ -9,6 +9,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.Spinner;
 
@@ -16,22 +17,30 @@ import static edu.stanford.cs147.educonnectapp.R.*;
 
 public class TeacherSubmissionActivity extends AppCompatActivity {
 
+    EditText description;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(layout.activity_teacher_submission);
+        description = findViewById(R.id.teacherDescriptionEt);
 
-//        Spinner spinner = findViewById(R.id.spinner_options2);
-//        // Create an ArrayAdapter using the string array and a default spinner layout
-//        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
-//                R.array.text_options_array, android.R.layout.simple_spinner_item);
-//        // Specify the layout to use when the list of choices appears
-//        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-//        // Apply the adapter to the spinner
-//        spinner.setAdapter(adapter);
+        Spinner spinner = findViewById(R.id.spinner_options);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.text_options_array, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
     }
 
     public void onTeacherSubmitClick(View v){
+        // Description text needs to be passed to "Submitted Activity"
+        String descriptionText = description.getText().toString().trim();
+        Intent passDescription = new Intent(this, SubmittedActivity.class);
+        passDescription.putExtra("descriptionText", descriptionText);
+        startActivity(passDescription);
+
         Intent myIntent = new Intent(getBaseContext(), TeacherSubmittedActivity.class);
         startActivity(myIntent);
     }
